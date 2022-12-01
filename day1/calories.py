e2d332d3330619e2e28da7a5e9a272b0661c6e2d
Jochen Deferme <jochen.deferme@gmail.com>
2022-12-01 14:48:23 +0100

    completed day1 part2

--- day1/calories.py
+++ day1/calories.py
@@ -1,24 +1,38 @@
 def count(file):
+    elves = get_elves(file)
+    return max(elves)
+
+
+def top3(file):
+    elves = get_elves(file)
+    elves.sort(reverse=True)
+
+    return elves[0] + elves[1] + elves[2]
+
+
+def get_elves(file):
     elves = []
     with open(file) as f:
         count = 0
         while True:
             line = f.readline()
             if not line:
+                elves += [count]
                 break
             if line != '\n':
                 count += int(line.strip())
             else:
                 elves += [count]
                 count = 0
-
-    return max(elves)
+    return elves
 
 
 def main():
     print("the answer is ")
     print(count("data/test_data"))
     print(count("data/real_data"))
+    print(top3("data/test_data"))
+    print(top3("data/real_data"))
 
 
 if __name__ == "__main__":
--- day1/tests/test_count.py
+++ day1/tests/test_count.py
@@ -1,8 +1,11 @@
 from day1.calories import *
 
+
 def test_part1():
     assert count("day1/data/test_data") == 24000
     assert count("day1/data/real_data") == 74711
 
-# def test_part2():
 
+def test_part2():
+    assert top3("day1/data/test_data") == 45000
+    assert top3("day1/data/real_data") == 209481
