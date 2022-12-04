f617e61013a0592ee1b095784732b04f93566d05
Jochen Deferme <jochen.deferme@gmail.com>
2022-12-04 12:38:29 +0100

    solved day4 part2

--- day4/pairs.py
+++ day4/pairs.py
@@ -16,10 +16,32 @@ def part1(file):
     return total
 
 
+def part2(file):
+    total = 0
+    with open(file) as f:
+        while True:
+            line = f.readline().strip()
+            if not line:
+                break
+            left, right = line.split(",")
+            ll, lr = left.split("-")
+            rl, rr = right.split("-")
+            ll, lr, rl, rr = int(ll), int(lr), int(rl), int(rr)
+            if rl <= lr <= rr \
+                    or rl <= ll <= rr \
+                    or ll >= rl and lr <= rr \
+                    or ll <= rl and lr >= rr:
+                total += 1
+
+    return total
+
+
 def main():
     print("the answer is ")
     print(part1("data/test_data"))
     print(part1("data/real_data"))
+    print(part2("data/test_data"))
+    print(part2("data/real_data"))
 
 
 if __name__ == "__main__":
--- day4/tests/test_count.py
+++ day4/tests/test_count.py
@@ -5,5 +5,5 @@ def test_part1():
     assert part1("day4/data/test_data") == 2
 
 
-# def test_part2():
-#     assert part2("day4/data/test_data") == 70
+def test_part2():
+    assert part2("day4/data/test_data") == 4
